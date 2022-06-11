653775395e0cf4f9d79a6c396169b9ff2c748458
xsone <xsone2@gmail.com>
2022-06-11 20:54:51 +0200

    First working version (c) june 2022 Jack Runs a server on PI with XMPP to Nefit/Bosch Easycontrol

---
+++ Watersensor-v3 (1).py
@@ -0,0 +1,58 @@
+import time
+import RPi.GPIO as GPIO
+
+waterInputPin = 23
+waterLedPin = 18
+waterLtr = 0
+waterM3 = 11
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(waterInputPin, GPIO.IN)
+GPIO.setup(waterLedPin, GPIO.OUT)
+
+print("Watersensor goed aangesloten op GPIO 23, pin 16?")
+
+GPIO.add_event_detect(waterInputPin, GPIO.RISING)  # add rising edge detection on a channel
+
+while True:
+    #if GPIO.input(waterInputPin):
+    if GPIO.event_detected(waterInputPin):    
+       waterLtr = waterLtr + 1
+       print("Water Liters: ", waterLtr)
+       GPIO.output(waterLedPin, GPIO.HIGH)
+       if (waterLtr >= 1000): #1000 pulsen = 1 M3
+                waterM3 = waterM3 + 1
+                print("WaterM3: ", waterM3)
+    else:
+    #   print('Input was LOW')
+       GPIO.output(waterLedPin, GPIO.LOW)
+       time.sleep(0.5)
+    
+    #GPIO.wait_for_edge(waterInputPin, GPIO.FALLING) # we wait for the button to be pressed
+    #if GPIO.input(waterInputPin, GPIO.HIGH):
+        #if pressed:
+           #print("Watersensor detect!: ", waterInt)
+           #pressed = True
+           # waterInt = waterInt + 1
+           # GPIO.output(waterLedPin, GPIO.HIGH)
+           # print("WaterInt: ", waterInt)
+           # if (waterInt >= 10): #Moet 2000 zijn, 1000 pulsen = 1 liter
+           #     waterLtr = waterLtr + 1
+           #     waterInt = 0
+           #     print("WaterLtr: ", waterLtr)
+    #else:
+     #  pressed = False
+      # GPIO.output(waterLedPin, GPIO.LOW)
+      # time.sleep(0.1)
+
+#Catch when script is interrupted, cleanup correctly
+#try:
+    # Main loop
+#    while True:
+#        print(rc_time(pin_to_circuit))
+#except KeyboardInterrupt:
+#    pass
+#finally:
+#    GPIO.cleanup()
+    
